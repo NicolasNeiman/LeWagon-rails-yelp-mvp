641d920397f1999214bf4f8fe18363aed8917d88
Nicolas Neiman <nicolasneiman@gmail.com>
2020-11-11 11:55:47 +0100

    add models with validations

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+CATEGORIES = %w[chinese italian japanese french belgian]
+
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+end
